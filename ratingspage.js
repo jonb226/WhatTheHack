fdd5c5aa2789a1194147fde0decaaba7af5a2c96
Gal <ghshenar@gmail.com>
2015-04-25 03:19:56 -0700

    node server

---
+++ ratingspage.js
@@ -0,0 +1,154 @@
+function getflickrimage(cityName){
+    
+    var resolve;
+    var reject;
+    var p = new Promise(function(res, rej){
+        resolve = res;
+        reject = rej;
+    });
+    
+    var placeSrcString = 'https://api.flickr.com/services/rest/?api_key=836e0029ba396aba560ba337f03bd4a0&method=flickr.places.find&query=' + cityName;
+    
+    fetch(placeSrcString).then(function(res){
+        var p = new Promise(function(resolve, reject){
+            if(res.status !== 200){
+                return false;
+            }
+            res.text().then(function(txt){
+                parser=new DOMParser();
+                xmlDoc=parser.parseFromString(txt,"text/xml");
+                var place = xmlDoc.getElementsByTagName('place')[0];
+                resolve(place.getAttribute('place_id'));
+            })
+        });
+        return p;    
+    }).then(function(place_id){
+        var srcString = 'https://api.flickr.com/services/rest/?api_key=836e0029ba396aba560ba337f03bd4a0&method=flickr.photos.search&place_id=' + place_id + '&tags=city';
+        return fetch(srcString);
+    }).then(function(res){
+        if(res.status !== 200){
+            return false;
+        }
+        res.text().then(function(txt){
+            parser=new DOMParser();
+            xmlDoc=parser.parseFromString(txt,"text/xml");
+            var photos = Array.prototype.slice.call(
+                xmlDoc.getElementsByTagName('photo'),0,3);
+            var urls = photos.map(function(e){
+                var farm = e.getAttribute('farm');
+                var id = e.getAttribute('id');
+                var secret = e.getAttribute('secret');
+                var server = e.getAttribute('server');
+                return 'https://farm' + farm + '.staticflickr.com/' + 
+                    server + '/' + id + '_' + secret + '.jpg';
+            });
+            resolve(urls);
+        });
+    });
+    
+    return p;
+}
+
+var loadNewCity = function(cityIndex) {
+	$(".carousel-image-1").attr("src", cities[cityIndex].imageUrl1);
+	$(".carousel-image-2").attr("src", cities[cityIndex].imageUrl2);
+	$(".carousel-image-3").attr("src", cities[cityIndex].imageUrl3);
+	$(".carousel-city-name").text(cities[cityIndex].name);
+	$(".destination-name-current").text(cityIndex + 1);
+
+	var rating1 = $(".rating-1");
+	var rating2 = $(".rating-2");
+	var rating3 = $(".rating-3");
+	var rating4 = $(".rating-4");
+	var rating5 = $(".rating-5");
+	rating1.click(function() {
+		cities[cityIndex].rating = 1;
+		if(cityIndex < cities.length) {
+			loadNewCity(cityIndex + 1);
+		}
+	});
+	rating2.click(function() {
+		cities[cityIndex].rating = 2;
+		if(cityIndex < cities.length) {
+			loadNewCity(cityIndex + 1);
+		}
+	});
+	rating3.click(function() {
+		cities[cityIndex].rating = 3;
+		if(cityIndex < cities.length) {
+			loadNewCity(cityIndex + 1);
+		}
+	});
+	rating4.click(function() {
+		cities[cityIndex].rating = 4;
+		if(cityIndex < cities.length) {
+			loadNewCity(cityIndex + 1);
+		}
+	});
+	rating5.click(function() {
+		cities[cityIndex].rating = 5;
+		if(cityIndex < cities.length) {
+			loadNewCity(cityIndex + 1);
+		}
+	});
+}
+
+var getImages = function() {
+	_.each(cities, function(city) {
+		if(city.name) {
+			var promise = getflickrimage(city.name);
+			promise.then(function(images) {
+				city.imageUrl1 = images[0];
+				city.imageUrl2 = images[1];
+				city.imageUrl3 = images[2];				
+			});
+		}
+	});
+}
+
+var getCities = function() {
+	$.ajax({
+	  url: "https://blooming-lake-7935.herokuapp.com/trip/2"
+	})
+	  .done(function( data ) {
+	    debugger;
+	});
+
+
+	var cities = [{
+		name: "Boston"
+	},{
+		name: "Miami",
+	},{
+		name: "Athens",
+	},{
+		name: "LA",
+	},{
+		name: "San Antonio",
+	},{
+		name: "New York",
+	},{
+		name: "Orlando",
+	},{
+		name: "New Orleans",
+	},{
+		name: "Grand Canyon",
+	},{
+		name: "San Francisco",
+	},{
+		name: "Seattle",
+	}];
+
+	return cities;
+}
+
+var start = function() {
+	$(".destination-name-total").text(cities.length);
+	loadNewCity(0);
+}
+
+var cities = getCities();
+getImages();
+setTimeout(function() {
+	start();
+}, 1000);
