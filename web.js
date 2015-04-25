eaf3d5f8cd2ddb81223fc5574084593b3102955c
Gal <ghshenar@gmail.com>
2015-04-25 03:27:47 -0700

    working

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 
 app.listen(app.get('port'), function() {
